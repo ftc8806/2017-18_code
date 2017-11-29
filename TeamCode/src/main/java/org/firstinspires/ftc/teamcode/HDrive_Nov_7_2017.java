fc1078627673046ef6468eb186e21adfd214603d
FTC8806 <32000192+ftc8806@users.noreply.github.com>
2017-11-28 20:28:19 -0500

    Trying to fix clamp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDrive_Nov_7_2017.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDrive_Nov_7_2017.java
@@ -12,34 +12,31 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "DRIVE PROGRAM")
 public class HDrive_Nov_7_2017 extends OpMode {
 
+    /* */
     DcMotor fl ,fr, bl, br, liftMotor;
     Servo theClamp;
     double power = 1;
 
+    /* STATIC VALUES */
+    final int LIFT_MAX_HEIGHT = 2900;
+
     @Override
     public void init() {
+        /* DEFINE THE MOTORS AND AND SERVOS */
         fl = hardwareMap.dcMotor.get("fl");
         fr = hardwareMap.dcMotor.get("fr");
         bl = hardwareMap.dcMotor.get("bl");
         br = hardwareMap.dcMotor.get("br");
         liftMotor = hardwareMap.dcMotor.get("lift");
         theClamp = hardwareMap.servo.get("clamp");
+
+        /* SET MODE ON LIFT MOTOR */
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
     public void loop() {
-        /*if(gamepad1.a){
-            fl.setPower(1);
-        } else if(gamepad1.b){
-            fr.setPower(1);
-        } else if(gamepad1.x){
-            bl.setPower(1);
-        } else if(gamepad1.y){
-            br.setPower(1);
-        } else {
-            stop();
-        }*/
-
         if(gamepad1.dpad_up){
             fl.setPower(-1);
             fr.setPower(1);
@@ -74,7 +71,10 @@ public class HDrive_Nov_7_2017 extends OpMode {
             stop();
         }
 
-        if(gamepad2.dpad_up){
+        /* CONTROL THE LIFT GOING UP AND DOWN */
+        if(liftMotor.getCurrentPosition() > LIFT_MAX_HEIGHT){
+            liftMotor.setPower(0);
+        } else if(gamepad2.dpad_up){
             liftMotor.setPower(0.5);
             telemetry.addData(">", "Lift going up");
         } else if (gamepad2.dpad_down){
@@ -85,17 +85,24 @@ public class HDrive_Nov_7_2017 extends OpMode {
             liftMotor.setPower(0);
         }
 
+        /* CLAMP OPENING CLOSING */
         if(gamepad2.x) {
             telemetry.addData(">", "close da clamp");
-            theClamp.setPosition(0.135);
+            theClamp.setPosition(0);
         } else if (gamepad2.y){
             telemetry.addData(">", "open da clamp");
-            theClamp.setPosition(0);
+            theClamp.setPosition(0.3);
         }
 
+        /* BUTTONS FOR PREDEFINED HEIGHTS */
+        //2900 max
+
+        /* TELEMETRY */
         telemetry.addData("!", "Use the dpad to drive!");
         telemetry.addData("!", "Use the bumpers to turn!");
         telemetry.addData("!", "Use start/back the change power!");
+        telemetry.addData("ServoPos", theClamp.getPosition());
+        telemetry.addData("LiftEncoderPos", liftMotor.getCurrentPosition());
         telemetry.update();
 
     }
